c327d73787aefc28577a5a3c657e3064eaabba63
nmkhans <nurmoin05@gmail.com>
2023-02-28 17:48:30 +0600

    filter todo functionality added

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,12 +4,35 @@ import Todo from "../Todo/Todo";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
 
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
+      {todos
+        .filter((todo) => {
+          const { status } = filters;
+          switch (status) {
+            case "complete":
+              return todo.completed;
+
+            case "incomplete":
+              return !todo.completed;
+
+            default:
+              return true;
+          }
+        })
+        .filter((todo) => {
+          const { colors } = filters;
+          if (colors.length > 0) {
+            return colors.includes(todo?.color);
+          } else {
+            return true;
+          }
+        })
+        .map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
     </div>
   );
 };
