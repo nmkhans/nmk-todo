476f188bb1f2f0ebbc83780dd1bb3281c3f21d5a
nmkhans <nurmoin05@gmail.com>
2023-02-28 17:53:41 +0600

    add todo functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import noteImage from "../../assets/notes.png";
 import plusImage from "../../assets/plus.png";
-import tickImage from "../../assets/double-tick.png"
+import tickImage from "../../assets/double-tick.png";
+import { useDispatch } from "react-redux";
+import { added } from "./../../redux/todos/action";
 
 const Header = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(added(input));
+    setInput("");
+  };
+
   return (
     <header>
       <div>
-        <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+        <form
+          onSubmit={handleSubmit}
+          className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+        >
           <img src={noteImage} className="w-6 h-6" alt="Add todo" />
           <input
+            onChange={handleInput}
+            value={input}
             type="text"
             placeholder="Type your todo"
             className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
@@ -22,11 +42,7 @@ const Header = () => {
 
         <ul className="flex justify-between my-4 text-xs text-gray-500">
           <li className="flex space-x-1 cursor-pointer">
-            <img
-              className="w-4 h-4"
-              src={tickImage}
-              alt="Complete"
-            />
+            <img className="w-4 h-4" src={tickImage} alt="Complete" />
             <span>Complete All Tasks</span>
           </li>
           <li className="cursor-pointer">Clear completed</li>
