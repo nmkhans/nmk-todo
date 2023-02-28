c580b12190cd23f9c4e56f6198ad33ea6475598c
nmkhans <nurmoin05@gmail.com>
2023-02-28 12:39:10 +0600

    reducer created for filters  actions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import todoReducer from "./todos/reducer";
+import filterReducer from "./filters/reducer";
 
 const rootReducer = combineReducers({
-  todos: todoReducer
+  todos: todoReducer,
+  filters: filterReducer
 })
 
 export default rootReducer;
